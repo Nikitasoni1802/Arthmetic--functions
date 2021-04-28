a3077cd7a0ec86a00273bae87aa2c0273a1e6ca8
Nikitasoni1802 <tso.nikita@orbitbiotech.com>
2021-04-28 16:39:22 +0530

    1st commit

---
+++ Downloading files.R
@@ -0,0 +1,25 @@
+# Downloading File
+download.file("https://raw.githubusercontent.com/nitinsharmagroup/SLE712_files/master/bioinfo_asst3_part1_files/mydata.tsv",
+              destfile = "mydata.tsv")
+x <- read.table("mydata.tsv")
+head(x) 
+str(x)
+
+x <- read.table("mydata.tsv", header = TRUE)
+head(x)
+str(x)
+
+x <- read.table("mydata.tsv", header = TRUE, stringsAsFactors = FALSE)
+head(x)
+str(x)
+
+x <- read.table("mydata.tsv", header = TRUE, stringsAsFactors = FALSE, row.names = 1) # 1st way to save same things
+head(x)
+str(x)
+
+x <- read.csv("mydata.tsv", sep="\t", stringsAsFactors = FALSE, row.names = 1) # 2nd ways to achienve same things
+head(x)
+str(x)
+
+save.image("mydata.RData") # save data
+load("myadata.RData") # load the data
